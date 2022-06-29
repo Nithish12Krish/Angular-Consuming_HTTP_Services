911815a27da900870e3fb74fb03c2d1d8b4e3861
Nithish12Krish <nithish1281999@gmail.com>
2022-06-29 12:25:16 +0530

    Angular Http Services

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { PostService } from './services/post.service'
+import { PostsComponent } from './posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    PostService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
